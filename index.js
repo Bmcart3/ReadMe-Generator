e0ad3e62da815dff8734313ac23c82f8cc2ef687
bmcart3 <bmcart3@gmail.com>
2020-05-22 15:24:39 -0400

    installed inquirer, added prompts to get user information

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-const questions = [
+var inquirer = require("inquirer");
+var fs = require("fs");
 
-];
 
-function writeToFile(fileName, data) {
-}
+inquirer.prompt([
+    {
+    message: "What is your GitHub username?",
+    name: "username"
+    },
+    {
+    message: "What is your email?",
+    name: "email"
+    },
+    {
+    message: "What is the URL to your project?",
+    name: "URL"
+    },
+    {
+    message: "What is your project name?",
+    name: "project"
+    },
+    {
+    message: "Write a short description of your project.",
+    name: "description"
+    },
+    {
+    type: "list",
+    message: "What kind of license does your project have?",
+    name: "license",
+    choices: ["MIT", "APACHE 2.0", "GPL v3", "None"]
+    }
+])
+.then(function(answers){
+    
+})
 
-function init() {
+// const questions = [
 
-}
+// ];
 
-init();
+// function writeToFile(fileName, data) {
+// }
+
+// function init() {
+
+// }
+
+// init();
