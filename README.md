edf14b1d8a8fa6841c4fd31c673b444c5fc15dc2
bmcart3 <bmcart3@gmail.com>
2020-05-22 15:57:29 -0400

    adding more questions, working on readme markup

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# ReadMe-Generator
+# Project Name
+
+https://img.shields.io/badge/<LABEL>-<MESSAGE>-<COLOR>
+
+# Description
+
+This is where the description answer would go...
+
+# Table of Contents
+
+- Installation
+- Usage
+- License
+- Contributing
+- Tests
+- Questions
+
+# Installation 
+
+
 
--- index.js
+++ index.js
@@ -28,10 +28,27 @@ inquirer.prompt([
     message: "What kind of license does your project have?",
     name: "license",
     choices: ["MIT", "APACHE 2.0", "GPL v3", "None"]
+    },
+    {
+    message: "What command should be run to install dependencies?",
+    name: "dependecies"
+    },
+    {
+    message: "What command should be run to run tests?",
+    name: "tests"
+    },
+    {
+    message: "What should the user know about using the repo?",
+    name: "userKnowledge"
+    },
+    {
+    message: "What should the user know about contributing to the repo?",
+    name: "contribute"
     }
 ])
 .then(function(answers){
-    
+    fs
+
 })
 
 // const questions = [
